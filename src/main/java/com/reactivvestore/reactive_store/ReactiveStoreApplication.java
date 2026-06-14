de94a7e8638ee71c4bdec6cb0d9714eb60d61ff9
Sunayana Rao <sunayanarao@Sunayanas-MacBook-Air.local>
2026-06-14 23:04:55 +0530

    chore: initial project setup

---
+++ src/main/java/com/reactivvestore/reactive_store/ReactiveStoreApplication.java
@@ -0,0 +1,13 @@
+package com.reactivvestore.reactive_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reactivvestore/reactive_store/ReactiveStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactivvestore.reactive_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
